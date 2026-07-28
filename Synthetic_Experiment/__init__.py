15db3c97cfd2b691c92fb0971d01d8b988be3b2d
Xin Zheng <zhengxin0817@outlook.com>
2026-07-28 17:01:08 +0800

    Create __init__.py

---
+++ Synthetic_Experiment/__init__.py
@@ -0,0 +1,5 @@
+"""Synthetic experiments for testing the ShadeTree motivation."""
+
+from .datasets import DATASET_NAMES, SyntheticData, generate_dataset
+
+__all__ = ["DATASET_NAMES", "SyntheticData", "generate_dataset"]
